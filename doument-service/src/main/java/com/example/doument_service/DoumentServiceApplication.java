4d64e5f3b6f0733f1ad09862a5c69b16b2a26759
DYashin36 <159531708+DYashin36@users.noreply.github.com>
2025-09-14 11:23:51 +0400

    skeleton

---
+++ doument-service/src/main/java/com/example/doument_service/DoumentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.doument_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoumentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoumentServiceApplication.class, args);
+	}
+
+}
---
+++ doument-service/src/test/java/com/example/doument_service/DoumentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doument_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoumentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
