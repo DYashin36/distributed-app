ad67f7c72cebabded9add3f193363e4acb906d97
DYashin36 <159531708+DYashin36@users.noreply.github.com>
2025-09-14 13:59:56 +0400

    changes to dockerfile and pom-s

--- discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurikaServer
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
-
 }
