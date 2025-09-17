7e1211548b5e0ef4a9afdc3e0fb486f8fe159f5b
DYashin36 <159531708+DYashin36@users.noreply.github.com>
2025-09-17 20:36:33 +0400

    1709

---
+++ auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.auth_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // отключаем CSRF полностью (важно для REST API)
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // login и register доступны всем
+                        .anyRequest().authenticated()           // остальное — только с токеном
+                );
+
+        return http.build();
+    }
+}
---
+++ auth-service/src/main/java/com/example/auth_service/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.auth_service.controllers;
+
+import com.example.auth_service.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+        // TODO: проверить пароль через UserDetailsService + БД
+        if ("user".equals(username) && "password".equals(password)) {
+            String token = jwtUtil.generateToken(username);
+            return ResponseEntity.ok(Map.of("token", token));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
+
